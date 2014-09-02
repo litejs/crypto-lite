4b75b3e0a22194f6f5874ca01bf4f6c0ef6adb19
Lauri Rooden <lauri@rooden.ee>
2014-09-02 23:47:38 +0300

    Fix padding, close #1

--- tests/run.js
+++ tests/run.js
@@ -11,10 +11,12 @@ describe("crypto-lite").
 		equal(crypto.sha1("abc1ö") , "ed9006416a835d01c054c9272c52a1885571a9fc").
 		equal(crypto.sha1("kkkkkkkkkkkkkkkkkkkkkkkkk") , "005cb6065579bb259bdc966dcc3d800b4c0e631b").
 		equal(crypto.sha1("efgfsfgjfkdslkeföl") , "3228c5ff9b3f008816452fd2c55b6063abfcebf3").
+		equal(crypto.sha1("GEThttps://weirdurl.ggnett.no/siteadmin/sites/1/placements") , "c2ad0bda24fbc0ecdc32f7cbdd2fdc8e9f276c99").
 
 	it ("should hash sha256", {_skip:1}).
 		equal(crypto.sha256("abc") , "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad").
 		equal(crypto.sha256("abc1ö") , "555944eb51d8305f11989edc037c0a0fa9999b470c327b3940377fd2c6507ab3").
+		equal(crypto.sha256("GEThttps://weirdurl.ggnett.no/siteadmin/sites/1/placements") , "f90406e3cb201bc7c485b846b9c1509085b702343ddca85159ee559e025b2e7f").
 
 	it ("should have hmac").
 		equal(crypto.hmac("sha1", "w", "q") , "697e084de5f14b207f78bd51e748fdd625e5d4e3").
@@ -22,6 +24,7 @@ describe("crypto-lite").
 
 		equal(crypto.hmac("sha256", "w", "q") , "aa0faac9b60e5d328675f33221327654ad791e2935a4ae12e94c1ac939afdf22").
 		equal(crypto.hmac("sha256", "w23e", "8d48be98c11eb482f08afbce6d1902b0f0c028f987d58a595fd7fbf365a3dcc95") , "1769221eb6c4f6e52bed133c64d1ba118ef72bfaf91b39f065e0aa43a4788031").
+		equal(crypto.hmac("sha256", "key", "GEThttps://weirdurl.ggnett.no/siteadmin/sites/1/placements") , "6d15c0ed95f53580e777ce36b4d96419632723ab42a17c758628a4aa000ffa1d").
 
 	it ("should hash pbkdf2 with sha1").
 		equal(crypto.pbkdf2Sync("password", "salt", 1, 20) , "0c60c80f961f0e71f3a9b524af6012062fe037a6").
